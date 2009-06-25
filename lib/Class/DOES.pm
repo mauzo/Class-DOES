67493ee0d89863eedb02c5927eb39bfc86dc3ff7
Ben Morrow <ben@morrow.me.uk>
2009-06-25 05:23:10 +0100

    Warnings for absent versions.

--- lib/Class/DOES.pm
+++ lib/Class/DOES.pm
@@ -1,12 +1,22 @@
 package Class::DOES;
 
-use warnings;
+use 5.006;
+
 use strict;
+use warnings;
+use warnings::register;
 
 use Scalar::Util qw/blessed/;
 
 our $VERSION = "1.00";
 
+sub warnif {
+    if (warnings::enabled()) {
+        warnings::warn($_[0]);
+    }
+}
+
+sub get_mro;
 sub get_mro {
     my ($class) = @_;
 
@@ -22,12 +32,29 @@ sub get_mro {
 }
 
 sub import {
-    my (undef, @roles) = @_;
     my $pkg = caller;
+
+    # there is an extra argument on the start
+    @_ % 2 or warnif 
+        "Odd number of arguments passed to Class::DOES.\n" .
+        "Did you forget to include the versions?";
+
+    my %does;
+    {
+        no warnings;
+        (undef, %does) = @_;
+    }
+    for (keys %does) {
+        $does{$_} or warnif "False value provided for ->DOES($_)";
+
+        $does{$_} =~ /^(?:\w+::)+\w+$/ and warnif 
+            "'$does{$_}' for ->DOES($_) looks like a package.\n" .
+            "Did you forget to include the versions?";
+    }
 
     no strict "refs";
 
-    %{"$pkg\::DOES"} = @roles;
+    *{"$pkg\::DOES"} = \%does;
     *{"$pkg\::DOES"} = sub {
         my ($obj, $role) = @_;
 
@@ -35,7 +62,8 @@ sub import {
         defined $class or $class = $obj;
 
         my %mro;
-        # yes, this is a list.
+        # Yes, this is a list. Shut up with your 'better written as
+        # $mro{}' nonsense.
         @mro{ (), get_mro $class } = ();
         for (keys %mro) {
             if (my $rv = ${"$_\::DOES"}{$role}) {
